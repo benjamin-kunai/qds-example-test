ce73c78c761880d286d97e6112d8870b9cec07a1
benjamin-kunai <benjamin.hybart@kunaico.com>
2025-04-01 16:02:07 -0400

    Updated packages and added app component

---
+++ src/components/app/app.tsx
@@ -0,0 +1,10 @@
+import { component$} from '@builder.io/qwik';
+import { Checkbox } from '@kunai-consulting/qwik';
+
+export default component$(() => {
+  return (
+    <></>
+  );
+});
+
+import { LuCheck } from '@qwikest/icons/lucide';
